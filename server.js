6c54562c00bd25179fb69ef4e79332075317edea
rachelschipull <rlschipull@gmail.com>
2022-06-27 22:42:10 -0400

    add port

--- server.js
+++ server.js
@@ -15,3 +15,7 @@ MongoClient.connect(dbConnectionString)
         db = client.db(dbName)
         collection = db.collection('quotes')
     })
+
+app.listen(process.env.PORT || PORT,  () => {
+    console.log ('Server is running on port')
+})
